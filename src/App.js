f75991ba5339a95e07ae7995621e379326915280
BabyD9V <idontwannauselineappforwork77@gmail.com>
2023-07-18 11:15:02 +0700

    ref

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Router, Routes, NavLink } from 'react-router-dom';
+import Contact from './Contact';
 
+
+export function Layout() {
+  return (
+    <nav className='bg-secondary p-2 mb-3 text-center '>
+      <NavLink to="/" className="link" style={({ isActive }) => {
+        return {
+          textDecoration: isActive ? "none" : "underline",
+
+        };
+      }}>Main</NavLink>
+      <NavLink to="/product" className="link"
+        style={({ isActive }) => {
+          return {
+            textDecoration: isActive ? "none" : "underline",
+
+          };
+        }}>Product</NavLink>
+
+      <NavLink to="/contact" className="link"
+        style={({ isActive }) => {
+          return {
+            textDecoration: isActive ? "none" : "underline",
+
+          };
+        }}
+      >
+        Contact Us
+
+      </NavLink>
+
+    </nav >
+  );
+
+}
+function Index() {
+  return (
+    <>
+      <Layout />
+      <h3>Hello</h3>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+function Product() {
+  return (
+    <>
+      <Layout />
+      <h3>Product</h3>
+      <button>Let's สีข้าว</button>
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      {/*  //if you wanna use URL you should create this Element and cover this Element all */}
+      <Container className='bg-light p-2 my-3' >
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/main' element={< Index />} />
+          <Route path='/product' element={< Product />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+
+
+
   );
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Layout } from "./App";
+
+function Contact() {
+    return (
+        <>
+            <Layout />
+            <h3>Ubon Ratchathani University</h3>
+            <div >
+                Ubon Ratchathani University
+                <br></br>
+                85 Sathonlamark Road
+
+                Warin Chamrap District
+                Ubon Ratchathani
+                <br></br>
+                Thailand 34190
+            </div>
+        </>
+    );
+}
+
+export default Contact;
