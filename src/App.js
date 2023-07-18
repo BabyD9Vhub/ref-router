7227ec5080681bdeac3a1191d09671b1aa27f5f6
BabyD9V <idontwannauselineappforwork77@gmail.com>
2023-07-18 12:08:25 +0700

    ref-router-new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Table } from 'react-bootstrap';
 import { BrowserRouter, Route, Router, Routes, NavLink } from 'react-router-dom';
 import Contact from './Contact';
+import { useRef } from 'react';
 
 
 export function Layout() {
@@ -40,21 +41,58 @@ export function Layout() {
 
 }
 function Index() {
+  const header1 = useRef();
   return (
     <>
       <Layout />
-      <h3>Hello</h3>
-      <Button>Click Me</Button>
+      <h3 ref={header1}>Hello</h3>
+      <Button onClick={() => { header1.current.innerHTML = "Do you wanna seekouw with me ?" }}>Click Me</Button>
     </>
   );
 }
 
 function Product() {
+  const table = useRef();
+  const tr = useRef([]);
+  const DeleteRow = (i) => {
+    const index = tr.current[i].rowIndex;
+    table.current.deleteRow(index);
+  }
+
+  const data = [
+    ["Iphone", 45000],
+    ["AirPod", 6000],
+    ["Macbook", 45000],
+    ["Ipad", 35000]
+  ];
   return (
     <>
       <Layout />
-      <h3>Product</h3>
-      <button>Let's สีข้าว</button>
+      <h4>Product</h4>
+      <Table striped border hover className='my-3 ' ref={table}>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, i) => {
+              return (
+                <tr ref={(el) => { tr.current[i] = el }} key={i}>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                  <td className='text-center'><button variant="danger" onClick={() => DeleteRow(i)}>
+                    Delete
+                  </button></td>
+                </tr>
+              );
+            })}
+
+        </tbody>
+      </Table>
     </>
   );
 }
